07bbed5277b26e5adf32e21b4487efc7ad707ab0
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-09 15:10:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ test = function ( check , max){
 };
 
 test ( 300 , 200);
+
+
+
+
+test2 = function ( min , max){
+
+if ( max < min) return Math.floor(Math.random() * min);
+else if ( min == max) return max ;
+else return Math.floor(Math.random() * max);
+
+};
+
+test2 ( 100 , 2);
