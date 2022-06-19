c0a86e41c82f8801c6ff7e0215c4c1b2f7cee5fb
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-19 18:17:08 +0300

    Update line-check.js

--- js/line-check.js
+++ js/line-check.js
@@ -1,5 +1,7 @@
-const function lineLengthTest (string, length) {
+const lineLengthTest = (string, length) =>{
   return string.length <= length;
-}
+};
 
 lineLengthTest(100, 200);
+
+const lineLengthTest = (check, max) =>
