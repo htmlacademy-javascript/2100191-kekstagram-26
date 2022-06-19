a625e1b0c83eb70443eb913341776907eac05906
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-19 18:23:51 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -27,10 +27,10 @@ const NAMES = [
 const PHOTO_OBJECT_COUNT = 25;
 //Создание массива комментариев
 const createCommentObject = (id) => ({
-    id,
-    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-    message: getRandomArrayElement(MESSAGE_TEXT),
-    name: getRandomArrayElement(NAMES),
+  id,
+  avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGE_TEXT),
+  name: getRandomArrayElement(NAMES),
 });
 
 const createComments = () => Array.from({length: 5}, (_, id) => createCommentObject(id+1))
