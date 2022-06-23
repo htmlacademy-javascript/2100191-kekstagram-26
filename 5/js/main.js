64d11c00f51d566aeead0ba96470c4cad8119430
Keks <keks@htmlacademy.ru>
2022-06-23 16:14:55 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,1 @@
+import './random_user_image.js';
---
+++ 5/js/random_user_image.js
@@ -0,0 +1,20 @@
+import {createPhotos} from './data.js';
+
+const pictures = document.querySelector('.pictures');
+const templateFragment = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const photoObj = createPhotos();
+
+const similarListFragment = document.createDocumentFragment();
+
+photoObj.forEach(({url, likes, comments}) => {
+  const picture = templateFragment.cloneNode(true);
+  picture.querySelector('.picture__img').setAttribute('src', url);
+  picture.querySelector('.picture__likes').textContent = likes;
+  picture.querySelector('.picture__comments').textContent = comments;  
+  similarListFragment.appendChild(picture);
+});
+
+pictures.appendChild(similarListFragment);
