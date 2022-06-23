71842e127ff67035bd76829009e055eacdde9cd0
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-23 16:09:24 +0300

    Update random_user_image.js

--- js/random_user_image.js
+++ js/random_user_image.js
@@ -2,8 +2,8 @@ import {createPhotos} from './data.js';
 
 const pictures = document.querySelector('.pictures');
 const templateFragment = document.querySelector('#picture')
-.content
-.querySelector('.picture');
+ .content
+ .querySelector('.picture');
 
 const photoObj = createPhotos();
 
@@ -11,7 +11,7 @@ const similarListFragment = document.createDocumentFragment();
 
 photoObj.forEach(({url, likes, comments}) => {
   const picture = templateFragment.cloneNode(true);
-  picture.querySelector('.picture__img').setAttribute('src', url)
+  picture.querySelector('.picture__img').setAttribute('src', url);
   picture.querySelector('.picture__likes').textContent = likes;
   picture.querySelector('.picture__comments').textContent = comments;  
   similarListFragment.appendChild(picture);
