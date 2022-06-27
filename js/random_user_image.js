45c218f708d92c8215f3dfdac7f832509d44e75b
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-27 19:24:26 +0300

    Update random_user_image.js

--- js/random_user_image.js
+++ js/random_user_image.js
@@ -1,3 +1,4 @@
+//import { showBigPicture } from './big-picture';
 const picturesElement = document.querySelector('.pictures');
 const templateFragment = document.querySelector('#picture').content.querySelector('.picture');
 
@@ -7,15 +8,13 @@ const makePhotoElement = ({url, likes, comments}) => {
   picture.querySelector('.picture__likes').textContent = likes;
   picture.querySelector('.picture__comments').textContent = comments.length;
   // и вот тут в следующем задании про открытие big picture надо будет добавить слушатель click
-  //picture.querySelector('.picture__img').addEventListener('click', function())
+  //picture.addEventListener('click', showBigPicture(photo)());
   return picture;
 };
 
 const renderPhotos = (photos) => {
   const similarListFragment = document.createDocumentFragment();
-
   photos.forEach((photo) => similarListFragment.appendChild(makePhotoElement(photo)));
-
   picturesElement.appendChild(similarListFragment);
 };
 export {renderPhotos};
