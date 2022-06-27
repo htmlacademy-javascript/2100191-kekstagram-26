323f4e104001c2925163307ec61a2f5bee4912b0
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-27 23:48:54 +0300

    Update big-picture.js

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,28 +1,26 @@
 const fullScreen = document.querySelector('.big-picture');
-const commentTemplate = fullScreen.content.querySelector('.social__comment');
-const commentList = fullScreen.content.querySelector('.social__comments');
+const commentList = fullScreen.querySelector('.social__comments');
+const commentTemplate = commentList.querySelector('.social__comment');
 
-function showBigPicture(photo) {
 
-  return function(url, likes, comments, description) {
-    fullScreen.removeClass('hidden');
-    fullScreen.querySelector('.big-picture__img').firstElementChild.setAttribute('src', url);
-    fullScreen.querySelector('.likes-count').textContent = likes;
-    fullScreen.querySelector('.comments-count').textContent = comments.lenght;
-    fullScreen.querySelector('.social__caption').textContent = description;
+const makeComment = ({avatar, name, message}) => {
+  const socialComment = commentTemplate.cloneNode(true);
+  socialComment.querySelector('social_picture').setAttribute('src', avatar);
+  socialComment.querySelector('social_picture').setAttribute('alt', name);
+  socialComment.querySelector('social__text').textContent = message;
+  return socialComment;
+};
 
-    const makeComment = (avatar, name, message) =>{
-      const socialComment = commentTemplate.cloneNode(true);
-      socialComment.querySelector('social_picture').setAttribute('src', avatar);
-      socialComment.querySelector('social_picture').setAttribute('alt', name);
-      socialComment.querySelector('social__text').textContent = message;
-      return socialComment;
-    };
+const showBigPicture = ({url, likes, comments, description}) => {
+  fullScreen.querySelector('.big-picture__img').firstElementChild.setAttribute('src', url);
+  fullScreen.querySelector('.likes-count').textContent = likes;
+  fullScreen.querySelector('.comments-count').textContent = comments.lenght;
+  fullScreen.querySelector('.social__caption').textContent = description;
 
-    const similarListFragment = document.createDocumentFragment();
-    comments.forEach((comment) => similarListFragment.appendChild(makeComment(comment)));
-    commentList.appendChild(similarListFragment);
-  };
-}
-
+  commentList.append(
+    comments.map(
+      makeComment(comment)
+    )
+  );
+};
 export {showBigPicture};
