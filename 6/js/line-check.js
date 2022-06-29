9b957df01b67819adc9a695fc75bcd2bdca91c55
Keks <keks@htmlacademy.ru>
2022-06-29 11:07:46 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/line-check.js
@@ -0,0 +1,3 @@
+const lineLengthTest = (string, length) => string.length <= length;
+
+lineLengthTest(100, 200);
---
+++ 6/js/main.js
@@ -0,0 +1,5 @@
+import './bg_close_button.js';
+import {createPhotos} from './data.js';
+import {renderPhotos} from './random_user_image.js';
+const photos = createPhotos();
+renderPhotos(photos);
