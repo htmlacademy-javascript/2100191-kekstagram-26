bb75de61546c3ee535dd9f3f96a99ef3e0632c0f
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-29 10:30:35 +0300

    Update big-picture.js

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,13 +1,13 @@
 const fullScreen = document.querySelector('.big-picture');
 const commentList = fullScreen.querySelector('.social__comments');
 const commentTemplate = commentList.querySelector('.social__comment');
-
+const body = document.querySelector('body');
 
 const makeComment = ({avatar, name, message}) => {
   const socialComment = commentTemplate.cloneNode(true);
-  socialComment.querySelector('social_picture').setAttribute('src', avatar);
-  socialComment.querySelector('social_picture').setAttribute('alt', name);
-  socialComment.querySelector('social__text').textContent = message;
+  socialComment.querySelector('.social__picture').setAttribute('src', avatar);
+  socialComment.querySelector('.social__picture').setAttribute('alt', name);
+  socialComment.querySelector('.social__text').textContent = message;
   return socialComment;
 };
 
@@ -17,10 +17,12 @@ const showBigPicture = ({url, likes, comments, description}) => {
   fullScreen.querySelector('.comments-count').textContent = comments.lenght;
   fullScreen.querySelector('.social__caption').textContent = description;
 
-  commentList.append(
-    comments.map(
-      makeComment(comment)
-    )
-  );
+  commentList.append(...comments.map(makeComment));
+
+  fullScreen.querySelector('.social__comment-count').classList.add('hidden');
+  fullScreen.querySelector('.comments-loader').classList.add('hidden');
+  body.classList.add('modal-open');
+  fullScreen.classList.remove('hidden');
 };
+
 export {showBigPicture};
