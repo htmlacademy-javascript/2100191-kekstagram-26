0a0dd5a6336b27ec06ae399f785254b7f5c09a8a
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-06-29 11:57:59 +0300

    Update bg_close_button.js

--- js/bg_close_button.js
+++ js/bg_close_button.js
@@ -3,20 +3,12 @@ const fullScreen = document.querySelector('.big-picture');
 
 const closeButton = document.querySelector('#picture-cancel');
 
-closeButton.addEventListener('click', ()=> {
-  fullScreen.classList.add('hidden');
-  fullScreen.querySelector('.social__comment-count').removeClass('hidden');
-  fullScreen.querySelector('.comments-loader').removeClass('hidden');
-  body.removeClass('modal-open');
-  fullScreen.reset();
-});
-
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
+const closeBP = () =>{
+  closeButton.addEventListener('click', ()=> {
     fullScreen.classList.add('hidden');
-    fullScreen.querySelector('.social__comment-count').removeClass('hidden');
-    fullScreen.querySelector('.comments-loader').removeClass('hidden');
     body.removeClass('modal-open');
-    fullScreen.reset();
-  }
-});
+    document.removeEventListener('keydown', );
+  });
+};
+
+export {closeBP};
