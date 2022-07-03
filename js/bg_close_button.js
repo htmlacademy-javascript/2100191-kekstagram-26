ad7bab73bc588e2879dc5a93e60cb906ae603181
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-03 15:03:36 +0300

    Update bg_close_button.js

--- js/bg_close_button.js
+++ js/bg_close_button.js
@@ -1,14 +1,40 @@
 const body = document.querySelector('body');
 const fullScreen = document.querySelector('.big-picture');
+const photoEdit = document.querySelector('.img-upload__overlay');
 
-const closeButton = document.querySelector('#picture-cancel');
+//полноэкранный режим
 
+const closeBigPicture = document.querySelector('#picture-cancel');
 const closeBP = () =>{
-  closeButton.addEventListener('click', ()=> {
+  closeBigPicture.addEventListener('click', ()=> {
     fullScreen.classList.add('hidden');
-    body.removeClass('modal-open');
+    body.classList.remove('modal-open');
     document.removeEventListener('keydown', );
   });
 };
 
+
+//загрузка фото и его настройка
+
+const uplFile = document.querySelector('#upload-file');
+const openPhotoEdit = () =>{
+  uplFile.addEventListener('change', () => {
+    photoEdit.classList.remove('hidden');
+    body.classList.add('modal-open');
+  });
+};
+
+const closeFileUpload = document.querySelector('.img-upload__cancel');
+const closeUplFile = () =>{
+  closeFileUpload.addEventListener('click', ()=> {
+    uplFile.innerHTML = '';
+    photoEdit.classList.add('hidden');
+    body.classList.remove('modal-open');
+  });
+};
+
+
 export {closeBP};
+export {openPhotoEdit};
+export {closeUplFile};
+
