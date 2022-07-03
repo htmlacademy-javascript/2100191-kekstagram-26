c205d5f9c2becba2efa48eca922a23cec0f931e3
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-03 15:03:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-import {closeBP} from './bg_close_button.js';
+import {closeBP, openPhotoEdit, closeUplFile} from './buttons.js';
+
+
 import {createPhotos} from './data.js';
 import {renderPhotos} from './random_user_image.js';
+
 const photos = createPhotos();
 renderPhotos(photos);
+
+
 closeBP();
+openPhotoEdit();
+closeUplFile();
 
