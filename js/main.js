359276bab5b99bb06e53f89319c91a7ea07a6780
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-03 17:23:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import './file-upload-validate.js';
 import {closeBP, openPhotoEdit, closeUplFile} from './buttons.js';
 import {createPhotos} from './data.js';
 import {renderPhotos} from './random_user_image.js';
