2a45893d427561badc86db035d44ca191175e09b
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-08 22:28:31 +0300

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,56 +1,54 @@
-import {hideBigPictureButton} from './big-picture.js';
+import { hideBigPictureButton, onCloseBigPicture } from './big-picture.js';
 
 const body = document.querySelector('body');
-const fullScreen = document.querySelector('.big-picture');
 const photoEdit = document.querySelector('.img-upload__overlay');
 const pictureCancel = document.querySelector('#picture-cancel');
 const uplFile = document.querySelector('#upload-file');
+const imgUploadForm = document.querySelector('.img-upload__form');
 const uploadCancel = document.querySelector('.img-upload__cancel');
 const loadMore = document.querySelector('.comments-loader');
 const shownCommentsCount = document.querySelector('.social__comment-count');
-let startCommentsLenght = 5;
+const newText = shownCommentsCount.childNodes[0];
+let startCommentsLength = 5;
 
 //полноэкранный режим
 const closeBigPicture = () => {
   pictureCancel.addEventListener('click', () => {
-    startCommentsLenght = 5;
+    startCommentsLength = 5;
     loadMore.classList.remove('hidden');
-    fullScreen.classList.add('hidden');
-    const maxComments = document.querySelectorAll('.social__comment').length;
-    shownCommentsCount.innerHTML = `${startCommentsLenght} из ${maxComments} комментариев`;
-    body.classList.remove('modal-open');
-    document.removeEventListener('keydown', hideBigPictureButton);
+    newText.textContent = `${startCommentsLength  } из `;
+    hideBigPictureButton();
+    document.removeEventListener('keydown', onCloseBigPicture);
   });
 };
-
 //загрузка фото и его настройка
-const openPhotoEdit = () => {
+const openPhotoEdit = () =>{
   uplFile.addEventListener('change', () => {
     photoEdit.classList.remove('hidden');
     body.classList.add('modal-open');
   });
 };
-const closeFileUpload = () => {
-  uploadCancel.addEventListener('click', () => {
-    uplFile.innerHTML = ' ';
+const closeFileUpload = () =>{
+  uploadCancel.addEventListener('click', ()=> {
+    imgUploadForm.reset();
     photoEdit.classList.add('hidden');
     body.classList.remove('modal-open');
   });
 };
-
 //загрузить еще
-const loadMorePictures = () => {
+const loadMorePictures = () =>{
   loadMore.addEventListener('click', () => {
-    startCommentsLenght += 5;
+    startCommentsLength += 5;
     const array = Array.from(document.querySelectorAll('.social__comment'));
-    const visComments = array.slice(0, startCommentsLenght);
+    const visComments = array.slice(0, startCommentsLength);
 
     visComments.forEach((el) => el.classList.remove('hidden'));
 
     const maxComments = document.querySelectorAll('.social__comment').length;
-    shownCommentsCount.textContent = `${startCommentsLenght} из ${maxComments} комментариев`;
 
-    if (startCommentsLenght === maxComments) {
+    newText.textContent = `${startCommentsLength  } из `;
+
+    if (startCommentsLength >= maxComments) {
       loadMore.classList.add('hidden');
     }
   });
