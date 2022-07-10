22689e5af50d3caf00d0e3e8d2982cab772b61b5
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-10 16:50:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,17 +15,3 @@ closeFileUpload();
 loadMorePictures();
 photoScale();
 changePhotoFilter();
-
-import './form_validator.js';
-import {closeBigPicture , openPhotoEdit, closeFileUpload, loadMorePictures} from './buttons.js';
-import {createPhotos} from './data.js';
-import {renderPhotos} from './random_user_image.js';
-
-const photos = createPhotos();
-renderPhotos(photos);
-
-
-closeBigPicture();
-openPhotoEdit();
-closeFileUpload();
-loadMorePictures();
