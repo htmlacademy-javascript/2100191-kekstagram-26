133d1e6520d0c751e09edc34602d6d1632e2776b
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-12 16:32:00 +0300

    Update effects_slider.js

--- js/effects_slider.js
+++ js/effects_slider.js
@@ -11,23 +11,51 @@ noUiSlider.create(sliderElement, {
   start: 0.5,
   step: 0.1,
   connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
 });
 
 sliderElement.noUiSlider.on('update', () => {
-  valueElement.value = sliderElement.noUiSlider.get();
+  if(changePhotoFilterForm.closest('#effect-marvin')){
+    valueElement.value = sliderElement.noUiSlider.get();
+    imgUploadPreview.style.filter = `invert(${sliderElement.noUiSlider.get()}%)`;
+  }
+  if(changePhotoFilterForm.closest('#effect-phobos')){
+    valueElement.setAttribute('value', `${sliderElement.noUiSlider.get()}px`);
+  }
+  else{
+    valueElement.setAttribute('value', sliderElement.noUiSlider.get());
+    if(changePhotoFilterForm.closest('#effect-chrome')){
+      imgUploadPreview.style.filter = `grayscale(${sliderElement.noUiSlider.get()})`;
+    }
+    if(changePhotoFilterForm.closest('#effect-sepia')){
+      imgUploadPreview.style.filter = `sepia(${sliderElement.noUiSlider.get()})`;
+    }
+    if(changePhotoFilterForm.closest('#effect-heat')){
+      imgUploadPreview.style.filter = `brightness(${sliderElement.noUiSlider.get()})`;
+    }
+  }
 });
-
 
 //перемены
 
-
-/*const changeFilter = (evt) => {
+const changeFilter = (evt) => {
+  sliderElement.classList.remove('hidden');
+  imgUploadPreview.className = 'img-upload__preview';
   if(evt.target.closest('#effect-none')){
-    imgUploadPreview.className = 'img-upload__effects  effects';
     imgUploadPreview.classList.add('effects__preview--none');
+    sliderElement.classList.add('hidden');
   }
   if(evt.target.closest('#effect-chrome')){
-    imgUploadPreview.className = 'img-upload__effects  effects';
     imgUploadPreview.classList.add('effects__preview--chrome');
     sliderElement.noUiSlider.updateOptions({
       range: {
@@ -39,7 +67,6 @@ sliderElement.noUiSlider.on('update', () => {
     });
   }
   if(evt.target.closest('#effect-sepia')){
-    imgUploadPreview.className = 'img-upload__effects  effects';
     imgUploadPreview.classList.add('effects__preview--sepia');
     sliderElement.noUiSlider.updateOptions({
       range: {
@@ -51,7 +78,6 @@ sliderElement.noUiSlider.on('update', () => {
     });
   }
   if(evt.target.closest('#effect-marvin')){
-    imgUploadPreview.className = 'img-upload__effects  effects';
     imgUploadPreview.classList.add('effects__preview--marvin');
     sliderElement.noUiSlider.updateOptions({
       range: {
@@ -63,7 +89,6 @@ sliderElement.noUiSlider.on('update', () => {
     });
   }
   if(evt.target.closest('#effect-phobos')){
-    imgUploadPreview.className = 'img-upload__effects  effects';
     imgUploadPreview.classList.add('effects__preview--phobos');
     sliderElement.noUiSlider.updateOptions({
       range: {
@@ -75,7 +100,6 @@ sliderElement.noUiSlider.on('update', () => {
     });
   }
   if(evt.target.closest('#effect-heat')){
-    imgUploadPreview.className = 'img-upload__effects  effects';
     imgUploadPreview.classList.add('effects__preview--heat');
     sliderElement.noUiSlider.updateOptions({
       range: {
@@ -93,72 +117,3 @@ const changePhotoFilter = () => {
 };
 
 export {changePhotoFilter};
-
-
-/*changePhotoFilterForm.addEventListener('change', (evt) => {
-
-  if(evt.target.closest('#effect-none')){
-    //
-  }
-
-  if(evt.target.closest('#effect-chrome')){
-    sliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 1
-      },
-      start: 0.5,
-      step: 0.1
-    });
-
-    if(evt.target.closest('#effect-sepia')){
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1
-        },
-        start: 0.5,
-        step: 0.1
-      });
-    }
-
-    if(evt.target.closest('#effect-marvin')){
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 100
-        },
-        start: 50,
-        step: 1
-      });
-    }
-
-    if(evt.target.closest('#effect-phobos')){
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 3
-        },
-        start: 1.5,
-        step: 0.1
-      });
-    }
-
-    if(evt.target.closest('#effect-heat')){
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 1,
-          max: 3
-        },
-        start: 1.5,
-        step: 0.1
-      });
-    }
-
-
-    sliderElement.noUiSlider.set(80);
-
-  }
-}
-); */
-
