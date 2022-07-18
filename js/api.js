6783e0387c51f06b1d11d73a70b4c987b7ad20d2
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-18 14:46:50 +0300

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,39 @@
+import {showAlert} from './util.js';
+
+
+const getData = (onSuccess) => {
+  fetch('https://26.javascript.pages.academy/kekstagram/data')
+    .then((response) => response.json())
+    .then((photos) => {
+      onSuccess(photos);
+    })
+    .catch(() => {
+      showAlert('Не удалось загрузить фотографии.Перезагрузите страницу');
+    });
+};
+
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    'https://26.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      body,
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch(() => {
+      onFail('Не удалось отправить форму. Попробуйте ещё раз');
+    });
+};
+
+export {getData, sendData};
