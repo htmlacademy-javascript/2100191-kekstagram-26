ceef131eed290653159081e2f46295dd983cdf8b
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-18 16:15:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 import './form_validator.js';
+import {showAlert} from './util.js';
 import {setUserFormSubmit} from './form_validator.js';
 import {changePhotoFilter} from './effects_sliders.js';
 import {getData} from './api.js';
 import {closeBigPicture , openPhotoEdit, closeFileUpload, loadMorePictures, photoScale} from './buttons.js';
 import {renderPhotos} from './random_user_image.js';
 
-getData((photos)=> {
-  renderPhotos(photos);
-});
+getData((photos)=> {renderPhotos(photos);},showAlert);
 
 closeBigPicture();
 openPhotoEdit();
@@ -16,5 +15,4 @@ loadMorePictures();
 photoScale();
 changePhotoFilter();
 
-setUserFormSubmit(closeFileUpload);
-
+setUserFormSubmit();
