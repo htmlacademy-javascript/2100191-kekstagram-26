bdf8eb299d3c7ecd411bcacdbba8b3091606d8a8
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-21 15:46:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 import './form_validator.js';
+import {choseFile} from './chosePhoto.js';
 import {setUserFormSubmit} from './form_validator.js';
 import {changePhotoFilter} from './effects_sliders.js';
 
 import {initButtonHandlers} from './buttons.js';
 
 import {getData} from './api.js';
-import {renderPhotos} from './random_user_image.js';
+import {renderPhotos, showAllPhotos, showRandomPhotos, showMostPopularPhotos} from './random_user_image.js';
 import {showAlert} from './util.js';
 
 getData((photos)=> {renderPhotos(photos);},showAlert);
+
+getData((photos) => {showRandomPhotos(photos);},showAlert);
+getData((photos) => {showAllPhotos(photos);},showAlert);
+getData((photos) => {showMostPopularPhotos(photos);},showAlert);
 
 initButtonHandlers();
 
@@ -16,3 +21,4 @@ changePhotoFilter();
 
 setUserFormSubmit();
 
+choseFile();
