18726f47fcf1f812229e45153b08e85c748ba054
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-21 15:47:17 +0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,5 +1,7 @@
 const ALERT_SHOW_TIME = 5000;
 
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -30,6 +32,13 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-const isEscapeKey = (evt) => evt.key === 'Escape';
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
 
-export {getRandomArrayElement, getRandomPositiveInteger, showAlert, isEscapeKey};
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomArrayElement, getRandomPositiveInteger, showAlert, debounce, isEscapeKey};
