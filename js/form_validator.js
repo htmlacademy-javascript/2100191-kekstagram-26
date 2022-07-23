eb1197db057823dba501f4116bb1e30aeb1119fb
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-23 18:53:25 +0300

    Update form_validator.js

--- js/form_validator.js
+++ js/form_validator.js
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-arrow-callback */
 import {sendData} from './api.js';
 import {showAlert, isEscapeKey} from './util.js';
 import {photoUploadCloseHandler} from './buttons.js';
