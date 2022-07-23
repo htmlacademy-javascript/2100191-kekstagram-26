2306f6e549d95863672254465f5ada1d621191dd
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-23 17:56:10 +0300

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,6 +1,10 @@
 import {hideBigPictureButton, onCloseBigPicture, onShowMoreComments} from './big-picture.js';
 import {isEscapeKey} from './util.js';
 
+const MIN_PHOTOS_SCALE = 25;
+const MAX_PHOTOS_SCALE = 100;
+const SCALE_STEP = 25;
+
 const body = document.querySelector('body');
 const photoEdit = document.querySelector('.img-upload__overlay');
 const pictureCancel = document.querySelector('#picture-cancel');
@@ -11,17 +15,18 @@ const loadMore = document.querySelector('.comments-loader');
 const scaleControlValue = document.querySelector('.scale__control--value');
 const uploadScale = document.querySelector('.img-upload__scale');
 const imgUploadPreview = document.querySelector('.img-upload__preview');
-const sliderElement = document.querySelector('.effect-level__slider');
+const userPic = document.querySelector('.setup-user-pic');
+const sliderForm = document.querySelector('.img-upload__effect-level');
 const changePhotoFilterForm = document.querySelector('.img-upload__effects');
 
 
 const onClosePhotoUpload = (e) => {
   if (isEscapeKey(e) || e.type === 'click') {
-    closePhotoUpload();
+    photoUploadCloseHandler();
   }
 };
 
-function closePhotoUpload () {
+function photoUploadCloseHandler () {
   imgUploadForm.reset();
   imgUploadPreview.className = 'img-upload__preview';
   scaleControlValue.setAttribute('value', '100%');
@@ -35,37 +40,37 @@ const onChangeSmallerBigger = (evt) => {
   const value = scaleControlValue.getAttribute('value');
   const cleanValue = value.replace(/\D/g,'');
   if (evt.target.closest('.scale__control--smaller')) {
-    if (cleanValue > 25){
-      scaleControlValue.setAttribute('value', `${Number(cleanValue) - 25}%`);
-      imgUploadPreview.style.transform = `scale( ${(Number(cleanValue) - 25)/100})`;
+    if (cleanValue > MIN_PHOTOS_SCALE){
+      scaleControlValue.setAttribute('value', `${Number(cleanValue) - SCALE_STEP}%`);
+      userPic.style.transform = `scale( ${(Number(cleanValue) - SCALE_STEP)/100})`;
     }
   }
   if (evt.target.closest('.scale__control--bigger')) {
-    if (cleanValue < 100){
-      scaleControlValue.setAttribute('value', `${Number(cleanValue) + 25}%`);
-      imgUploadPreview.style.transform = `scale( ${(Number(cleanValue) + 25)/100})`;
+    if (cleanValue < MAX_PHOTOS_SCALE){
+      scaleControlValue.setAttribute('value', `${Number(cleanValue) + SCALE_STEP}%`);
+      userPic.style.transform = `scale( ${(Number(cleanValue) + SCALE_STEP)/100})`;
     }
   }
 };
 
 const initButtonHandlers = () => {
-  pictureCancel.addEventListener('click', () => {
+  pictureCancel.addEventListener('click', function onPictureCancel () {
     hideBigPictureButton();
     document.removeEventListener('keydown', onCloseBigPicture);
   });
 
-  uplFile.addEventListener('change', () => {
+  uplFile.addEventListener('change', function onUploadFile () {
     photoEdit.classList.remove('hidden');
     body.classList.add('modal-open');
 
     if(changePhotoFilterForm.querySelector('.effects__radio:checked').value === 'none'){
-      sliderElement.classList.add('hidden');
+      sliderForm.classList.add('hidden');
     }
 
     document.addEventListener('keydown', onClosePhotoUpload);
   });
 
-  uploadCancel.addEventListener('click', closePhotoUpload);
+  uploadCancel.addEventListener('click', photoUploadCloseHandler);
 
   loadMore.addEventListener('click', onShowMoreComments);
 
@@ -73,4 +78,4 @@ const initButtonHandlers = () => {
 };
 
 
-export {initButtonHandlers, closePhotoUpload};
+export {initButtonHandlers, photoUploadCloseHandler};
