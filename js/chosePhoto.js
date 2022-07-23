eeb6a95bcde4081334eef36c42b29b38be809813
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-23 17:55:53 +0300

    Update chosePhoto.js

--- js/chosePhoto.js
+++ js/chosePhoto.js
@@ -4,11 +4,12 @@ const fileChooser = document.querySelector('#upload-file');
 const preview = document.querySelector('.setup-user-pic');
 
 const choseFile = () => {
-  fileChooser.addEventListener('change', () => {
+  fileChooser.addEventListener('change', function onFileChose () {
     const file = fileChooser.files[0];
     const fileName = file.name.toLowerCase();
 
-    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+    const matches = FILE_TYPES.some((it) => {
+      return fileName.endsWith(it);});
 
     if (matches) {
       preview.src = URL.createObjectURL(file);
